346f2527ca80b8af197819a93e10acc4fbe35c5b
Wyrizon 0xY <108142718+ScarletDeveloper@users.noreply.github.com>
2024-11-29 21:51:04 +0100

    Update RedReaper.py

--- RedReaper.py
+++ RedReaper.py
@@ -6,7 +6,6 @@ class Cobrelet:
     def __init__(self):
         self.char_map = {
             'a': 'af4', 'b': 'b5z', 'c': 'k9', 'd': 'd3x', 'e': 'e7y',
-            # (a teljes char_map itt folytatódik)
         }
 
     def transform_data(self, data):
@@ -17,7 +16,6 @@ class Cobrelet:
         """Hash the input data using the specified algorithm."""
         transformed_data = self.transform_data(data).encode('utf-8')
         
-        # Hashing according to the selected algorithm
         if algorithm == 'sha256':
             return hashlib.sha256(transformed_data).hexdigest()
         elif algorithm == 'sha1':
@@ -31,28 +29,25 @@ class Cobrelet:
 
     def brute_force(self, hash_value, algorithm='sha256', max_length=12):
         """Brute-force attack to find the original string that hashes to the given hash."""
-        chars = string.ascii_letters + string.digits + string.punctuation  # Character set
-        total_attempts = 0  # Counter for total attempts
+        chars = string.ascii_letters + string.digits + string.punctuation
+        total_attempts = 0  
         total_combinations = sum(len(chars) ** i for i in range(1, max_length + 1))
 
         for length in range(1, max_length + 1):
             for attempt in itertools.product(chars, repeat=length):
                 attempt_string = ''.join(attempt)
-                total_attempts += 1  # Increment attempts counter
+                total_attempts += 1
 
-                # Print the attempt
                 print(f"Attempt {total_attempts}/{total_combinations}: {attempt_string}")
 
-                # Hash the current attempt and compare
                 if hashlib.sha256(attempt_string.encode('utf-8')).hexdigest() == hash_value:
                     print(f"Found '{attempt_string}'!")
-                    return attempt_string  # Return the found password immediately
+                    return attempt_string
 
         print("Failed to crack the hash.")
-        return None  # If no match found
+        return None
 
 def main():
-    # ANSI escape codes for red color
     red_color = "\033[31m"
     reset_color = "\033[0m"
     
@@ -63,14 +58,12 @@ def main():
     print(f"{red_color}██║  ██║███████╗██████╔╝    ██║  ██║███████╗██║  ██║██║     ███████╗██║  ██║")
     print(f"{red_color}╚═╝  ╚═╝╚══════╝╚═════╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝{reset_color}")
 
-    # User input for the hash and algorithm
     hash_to_crack = input("Enter the hash to crack: ")
     algorithm = input("Enter the hash algorithm (sha256, sha1, md5, sha512): ")
 
-    # Create an instance of Cobrelet
     cobrelet = Cobrelet()
 
-    # Brute force the hash
+
     cobrelet.brute_force(hash_to_crack, algorithm)
 
     input("Press Enter to exit...")
